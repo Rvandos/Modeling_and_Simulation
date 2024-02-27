668b7dcc555ca4bdf3460572f8421c78eebede4a
Rvandos <vandosselaar.rick@gmail.com>
2024-02-27 10:25:18 +0100

    Placeholder results, interpretation notebooks and initial main files
    A results folder to store simulated results is constructed. For each simulation, the f1 and accuracy draws will be stored as pickle files.
    
    Additionally two main files have been constructed to perform the simulations:
    -main_bootstrap: performs the regular bootstrap simulation.
    -main_logistic_bootstrap: performs bootstrap on a newly created population with linear dependencies between features and y.

--- Modeling_and_Simulation/main_logistic_bootstrap.py
+++ Modeling_and_Simulation/main_logistic_bootstrap.py
@@ -1,4 +1,5 @@
-"""This is the main script that performs the execution of the basic bootstrap simulation!
+"""This is the main script that performs the execution of the logistic bootstrap simulation. A new population 
+is created, where a linear relationship is imposed between the set of features and the dependent variable!
 """
 import numpy as np
 import pandas as pd
@@ -9,7 +10,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score, accuracy_score
 
-from utils import bootstrap
+from utils import bootstrap, create_logistic_population
 
 
 #Set and initialize required global variables
@@ -22,10 +23,10 @@ logreg_f1_list = []
 nn_acc_list = []
 nn_f1_list = []
 
+POPULATION_LOGISTIC_PARAMETERS = {}
 LOGISTIC_PARAMETERS = {'random_state': 2024272}
 NEURAL_NETWORK_PARAMETERS = {}
 TEST_SIZE = 0.3
-
 
 
 #Set seed 
@@ -45,15 +46,18 @@ col_order = ['Churn'] + [col for col in column_names if col != 'Churn']
 df = df[col_order]
 X = df.to_numpy()
 
+#Create a new population using a logistic regression:
+X_new = create_logistic_population(X, POPULATION_LOGISTIC_PARAMETERS)
 
-#Perform the simulation using the specified model:
-#-----------------------------------------------------------------------------
+
+#Perform the simulation on the newly generated population:
 for b in range(N_SIM):
     #Obtain a bootstrap sample
-    x_b = bootstrap(X, N_OBS)
+    xb_new = bootstrap(X_new, N_OBS)
 
     #Make a train-test split
-    xb_train, xb_test, yb_train, yb_test = train_test_split(x_b[:,1:], x_b[:,0], test_size= TEST_SIZE)
+    xb_train, xb_test, yb_train, yb_test = train_test_split(xb_new[:,1:], xb_new[:,0], test_size= TEST_SIZE)
+
 
     #Logistic Regression:
     #Train the model using the train set:
@@ -76,9 +80,8 @@ for b in range(N_SIM):
 
 
 
-
 #Store the results in a seperate folder
-storage_loc = os.path.join(os.getcwd(),'Modeling_and_Simulation', 'Results', 'bootstrap')
+storage_loc = os.path.join(os.getcwd(),'Modeling_and_Simulation', 'Results', 'logistic_bootstrap')
 
 #Store logistic regression results
 with open(storage_loc + '/logistic_regression_accuracy.pkl', 'wb') as f:
@@ -90,6 +93,9 @@ with open(storage_loc + '/neural_network_accuracy.pkl', 'wb') as f:
 with open(storage_loc + '/neural_network_f1.pkl', 'wb') as f:
     pickle.dump(nn_f1_list, f)
 
-
+#Store the newly generated population
+with open(storage_loc + '/new_population.csv', 'w') as f:
+    pd.DataFrame(X_new, columns = col_order).to_csv(f, index = False)
 
 print('The execution has finished! See the folder "Results" for the simulation results!')
+
